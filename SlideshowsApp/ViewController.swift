266ac215fe24f6e432cb569a3d4137ed3f933635
r-minowa <r-minowa@ienter.co.jp>
2020-04-13 15:25:41 +0900

    Initial Commit

---
+++ SlideshowsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowsApp
+//
+//  Created by 蓑輪 竜輝 on 2020/04/13.
+//  Copyright © 2020 ryuki.minowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
